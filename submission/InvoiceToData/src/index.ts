373ca4bdba26c78cca17413dcfbb18a19a7e4107
Seokki-Kwon <rnjstjrrl12@gmail.com>
2025-04-21 23:02:25 +0900

    [feat] invoice.pdf 데이터 추출

--- submission/InvoiceToData/src/index.ts
+++ submission/InvoiceToData/src/index.ts
@@ -1,14 +1,44 @@
-import { GoogleGenAI } from "@google/genai";
 import 'dotenv/config';
-
-const ai = new GoogleGenAI({ apiKey: process.env.API_KEY });
-
-async function main() {
-  const response = await ai.models.generateContent({
-    model: "gemini-2.0-flash",
-    contents: "Explain how AI works in a few words",
-  });
-  console.log(response.text);
-}
-
-main();
+import {
+    GoogleGenAI,
+    createUserContent,
+    createPartFromUri,
+    Type,
+  } from "@google/genai";
+  
+  const ai = new GoogleGenAI({ apiKey: process.env.API_KEY });
+  
+  async function main() {
+    const myfile = await ai.files.upload({
+      file: __dirname + "/invoice.pdf",
+      config: { mimeType: "application/pdf" },
+    });
+  
+    const response = await ai.models.generateContent({
+      model: "gemini-2.5-flash-preview-04-17",
+      contents: createUserContent([
+        createPartFromUri(myfile.uri!, myfile.mimeType!),
+        "Describe this audio clip",
+      ]),
+      config: {
+        responseMimeType: 'application/json',
+        responseSchema: {          
+          type: Type.ARRAY,
+          items: {
+            type: Type.OBJECT,
+            properties: {
+              'invoiceNumber':{
+                type: Type.STRING,
+                description: 'invoice number',
+                nullable: false
+              }
+            },
+            required: ['invoiceNumber']
+          }
+        }
+      }
+    });
+    console.log(response.text);
+  }
+  
+  main();
